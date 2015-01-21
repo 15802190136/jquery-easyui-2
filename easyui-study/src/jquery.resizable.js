737f824f99abacbfada15859e9c35cb0435650f4
hjl <416921332@qq.com>
2015-01-21 22:36:31 +0800

    整理代码

--- easyui-study/src/jquery.resizable.js
+++ easyui-study/src/jquery.resizable.js
@@ -1,62 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>resizable_simple</title>
-  <style>
-body {
-  font-family: verdana,helvetica,arial,sans-serif;
-  padding: 20px;
-  font-size: 12px;
-  margin: 0;
+(function($) {
+$.fn.resizable = function(options, param){
+  if (typeof options == 'string'){
+    return $.fn.resizable.methods[options](this, param);
+  }
+  return this.each(function(){
+    var opts = null;
+    var state = $.data(this, 'resizable');
+    if (state) {
+      $(this).unbind('.resizable');
+      opts = $.extend(state.options, options || {});
+    }else{
+      opts = $.extend({}, $.fn.resizable.defaults, $.fn.resizable.parseOptions(this), options || {});
+      $.data(this, 'resizable', {
+        options:opts
+      });
+    }
+
+    if (opts.disabled == true) {
+      return;
+    }
+    bindEvents(this)
+  })
 }
-  </style>
-  <script src="../bower_components/jquery/jquery.js"></script>
-</head>
-<body>
-  <div class="easyui-resizable" data-options="minWidth:50,minHeight:50" style="width:200px;height:150px;border:1px solid #ccc;">
-    <div style="padding:20px">Resize Me</div>
-  </div>
-</body>
-<script src="../src/jquery.parser.js"></script>
-<script>
-$(function(){
-var target = $(".easyui-resizable")[0]
-var t = $(".easyui-resizable")
-var defaults = {
+$.fn.resizable.defaults = {
   disabled:false,
   handles:'n, e, s, w, ne, se, sw, nw, all',
   minWidth: 10,minHeight: 10,
   maxWidth: 10000,maxHeight: 10000,
   edge:5
 }
-var state_opts
-var isResizing = false
-main = function(){
-  parseOptions()
-  if (state_opts.disabled == true) return
-  bindEvents()
+$.fn.resizable.isResizing = false;
+$.fn.resizable.parseOptions = function(target){
+  var t = $(target);
+  return $.extend({},
+      $.parser.parseOptions(target, [
+        'handles',{minWidth:'number',minHeight:'number',maxWidth:'number',maxHeight:'number',edge:'number'}
+      ]), {
+    disabled: (t.attr('disabled') ? true : undefined)
+  })
 }
-parseOptions = function(){
-  state_opts = $.extend({}, defaults, $.parser.parseOptions(target, ['handle',
-    {minWidth:'number',minHeight:'number',maxWidth:'number',maxHeight:'number',edge:'number'}]), {
-      disabled: (t.attr('disabled') ? true : undefined)
-    });
-}
-bindEvents = function(){
-  t.bind('mousemove.resizable',function(e){
-    if (isResizing) return
+function bindEvents(target){
+  var t = $(target);
+  t.bind('mousemove.resizable',{target:target},function(e){
+    if ($.fn.resizable.isResizing) return
     var dir = getDirection(e);
     if (dir == '')
-      t.css('cursor', '');
+      $(e.data.target).css('cursor', '');
     else
-      t.css('cursor', dir + '-resize');
-  }).bind('mouseleave.resizable',function(e){
-    t.css('cursor', '');
-  }).bind('mousedown.resizable',function(e){
+      $(e.data.target).css('cursor', dir + '-resize');
+  }).bind('mouseleave.resizable',{target:target},function(e){
+    $(e.data.target).css('cursor', '');
+  }).bind('mousedown.resizable',{target:target},function(e){
     var dir = getDirection(e);
+    var t = $(e.data.target);
     if (dir == '') return;
+
+    function getCssValue(css) {
+      var val = parseInt(t.css(css));
+      if (isNaN(val))
+        return 0;
+      else
+        return val; 
+    }
     var data = {
+      target: e.data.target,
       dir: dir,
       startLeft: getCssValue('left'),
       startTop: getCssValue('top'),
@@ -77,12 +84,14 @@ bindEvents = function(){
     $('body').css('cursor', dir+'-resize');
   })
 }
-getDirection = function(e){
+function getDirection(e){
+  var t = $(e.data.target);
+  var opts = $.data(e.data.target,"resizable").options;
   var dir = '';
   var offset = t.offset();
   var width = t.outerWidth();
   var height = t.outerHeight();
-  var edge = state_opts.edge;
+  var edge = opts.edge;
   if (e.pageY > offset.top && e.pageY < offset.top + edge)
     dir += 'n';
   else if (e.pageY < offset.top + height && e.pageY > offset.top + height - edge)
@@ -92,7 +101,7 @@ getDirection = function(e){
   else if (e.pageX < offset.left + width && e.pageX > offset.left + width - edge)
     dir += 'e';
   
-  var handles = state_opts.handles.split(',');
+  var handles = opts.handles.split(',');
   for(var i=0; i<handles.length; i++) {
     var handle = handles[i].replace(/(^\s*)|(\s*$)/g, '');
     if (handle == 'all' || handle == dir)
@@ -100,38 +109,32 @@ getDirection = function(e){
   }
   return '';
 }
-getCssValue = function(css){
-  var val = parseInt(t.css(css));
-  if (isNaN(val))
-    return 0;
-  else
-    return val; 
-}
-doDown = function(e){
-  isResizing = true
+function doDown(e){
+  $.fn.resizable.isResizing = true;
   return false;
 }
-doMove = function(e){
+function doMove(e){
   resize(e);
   applySize(e);
-  return false;
+  return false;  
 }
-doUp = function(e){
-  isResizing = false;
+function doUp(e){
+  $.fn.resizable.isResizing = false;
   resize(e, true);
   applySize(e);
   $(document).unbind('.resizable');
   $('body').css('cursor','');
   return false;
 }
-resize = function(e){
+function resize(e){
   var resizeData = e.data;
+  var opts = $.data(resizeData.target, 'resizable').options;
   //右
   if (resizeData.dir.indexOf('e') != -1) {
     var width = resizeData.startWidth + e.pageX - resizeData.startX;
     width = Math.min(
-      Math.max(width, state_opts.minWidth),
-      state_opts.maxWidth
+      Math.max(width, opts.minWidth),
+      opts.maxWidth
     );
     resizeData.width = width;
   }
@@ -139,8 +142,8 @@ resize = function(e){
   if (resizeData.dir.indexOf('s') != -1) {
     var height = resizeData.startHeight + e.pageY - resizeData.startY;
     height = Math.min(
-      Math.max(height, state_opts.minHeight),
-      state_opts.maxHeight
+      Math.max(height, opts.minHeight),
+      opts.maxHeight
     );
     resizeData.height = height;
   }
@@ -148,8 +151,8 @@ resize = function(e){
   if (resizeData.dir.indexOf('w') != -1) {
     var width = resizeData.startWidth - e.pageX + resizeData.startX;
     width = Math.min(
-      Math.max(width, state_opts.minWidth),
-      state_opts.maxWidth
+      Math.max(width, opts.minWidth),
+      opts.maxWidth
     );
     resizeData.width = width;
     resizeData.left = resizeData.startLeft + resizeData.startWidth - resizeData.width;    
@@ -158,23 +161,21 @@ resize = function(e){
   if (resizeData.dir.indexOf('n') != -1) {
     var height = resizeData.startHeight - e.pageY + resizeData.startY;
     height = Math.min(
-      Math.max(height, state_opts.minHeight),
-      state_opts.maxHeight
+      Math.max(height, opts.minHeight),
+      opts.maxHeight
     );
     resizeData.height = height;
     resizeData.top = resizeData.startTop + resizeData.startHeight - resizeData.height;
   }
 }
-applySize = function(e){
+function applySize(e){
   var resizeData = e.data;
+  var t = $(resizeData.target);
   t.css({
     left: resizeData.left,
     top: resizeData.top
   });
   if (t.outerWidth() != resizeData.width){t._outerWidth(resizeData.width)}
   if (t.outerHeight() != resizeData.height){t._outerHeight(resizeData.height)}  
-}
-main();
-})
-</script>
-</html>
+}  
+})(jQuery);
